644a0c8ee8525e69743ee9851cd47bb73403cc3e
tolgaasahinn <97322196+tolgaasahinn@users.noreply.github.com>
2022-05-25 22:59:55 +0300

    SQL to postgres

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def load_user(user_id):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
